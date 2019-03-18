7b44013a5813ac0505129050030b2295307b66a6
Jack Massey <jmassey@barrukka.local>
2019-03-18 15:44:36 +1000

    uart - Fixed some compile warnings

--- src/uart.h
+++ src/uart.h
@@ -12,12 +12,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
-const FILE uart_stdout;
-const FILE uart_input;
-const FILE uart_io;
+static FILE uart_stdout;
+static FILE uart_input;
+static FILE uart_io;
 
 void uart_init(uint32_t baud);
 char uart_getc(void);
 void uart_sendc(char data);
-char uart_getchar(FILE * stream);
+int uart_getchar(FILE * stream);
 int uart_printchar(char var, FILE * stream);
