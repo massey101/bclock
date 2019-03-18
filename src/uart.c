7bc6b16bfa49e1ef423fc808687f30ea00e349fe
Jack Massey <jmassey@barrukka.local>
2019-03-18 17:36:26 +1000

    uart - Fixed formatting

--- src/uart.c
+++ src/uart.c
@@ -4,67 +4,71 @@
 #include "uart.h"
 
 static FILE uart_stdout = FDEV_SETUP_STREAM(
-        uart_printchar,
-        NULL,
-        _FDEV_SETUP_WRITE
+    uart_printchar,
+    NULL,
+    _FDEV_SETUP_WRITE
 );
 
 static FILE uart_input = FDEV_SETUP_STREAM(
-        NULL,
-        uart_getchar,
-        _FDEV_SETUP_READ
+    NULL,
+    uart_getchar,
+    _FDEV_SETUP_READ
 );
 
 static FILE uart_io = FDEV_SETUP_STREAM(
-        uart_printchar,
-        uart_getchar,
-        _FDEV_SETUP_RW
+    uart_printchar,
+    uart_getchar,
+    _FDEV_SETUP_RW
 );
 
 void uart_init(uint32_t baud)
 {
-	/* Set Baud rate */
-	UBRR0 = F_CPU/16/baud-1;
-	/* Set frame format: 8 bit data */
-	UCSR0C=(3<<UCSZ00);
-	/* Enable The receiver and transmitter */
-	UCSR0B=(1<<RXEN0)|(1<<TXEN0);
+    /* Set Baud rate */
+    UBRR0 = F_CPU/16/baud-1;
+    /* Set frame format: 8 bit data */
+    UCSR0C=(3<<UCSZ00);
+    /* Enable The receiver and transmitter */
+    UCSR0B=(1<<RXEN0)|(1<<TXEN0);
 }
 
 
 char uart_getc()
 {
-	/* Wait untill a data is available */
-	while(!(UCSR0A & (1<<RXC0)));
-	/* Return data */
-	return UDR0;
+    /* Wait untill a data is available */
+    while(!(UCSR0A & (1<<RXC0)));
+
+    /* Return data */
+    return UDR0;
 }
 
 
 void uart_sendc(char data)
 {
-	/* Wait untill the transmitter is ready */
-	while(!(UCSR0A & (1<<UDRE0)));
-	/* Send Data */
-	UDR0 = data;
+    /* Wait untill the transmitter is ready */
+    while(!(UCSR0A & (1<<UDRE0)));
+
+    /* Send Data */
+    UDR0 = data;
 }
 
 int uart_getchar(FILE * stream) {
-        char c = uart_getc();
-        /* Give return line endings. */
-        if (c == '\r') {
-            return '\n';
-        }
-        return c;
+    char c = uart_getc();
+    /* Give return line endings. */
+    if (c == '\r') {
+        return '\n';
+    }
+
+    return c;
 }
 
 int uart_printchar(char var, FILE * stream)
 {
-	/* Give return line endings. */
-	if (var == '\n') {
-		uart_sendc('\r');
-	}
-	/* Send character */
-	uart_sendc(var);
-	return 0;
+    /* Give return line endings. */
+    if (var == '\n') {
+        uart_sendc('\r');
+    }
+
+    /* Send character */
+    uart_sendc(var);
+    return 0;
 }
